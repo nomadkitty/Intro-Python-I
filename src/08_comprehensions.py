7a6e979e87547030a6bccd98371ef38e923d8b86
Jojo Zhang <jojozhang0327@gmail.com>
2020-06-04 01:17:50 -0400

    Update list compreshension from for loops

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,18 +10,18 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
-for item in range(6):
-    if item > 0:
-        y.append(item)
+y = [i for i in range(6)]
+# for item in range(6):
+#     if item > 0:
+#         y.append(item)
 print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
-for item in range(10):
-    y.append(item**3)
+y = [i**3 for i in range(10)]
+# for item in range(10):
+#     y.append(item**3)
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
@@ -29,9 +29,9 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
-for item in a:
-    y.append(item.upper())
+y = [i.upper() for i in a]
+# for item in a:
+#     y.append(item.upper())
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
